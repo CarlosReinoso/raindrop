81c394ed095f3441a083fd709dc7614bcd2f15ad
Carlos Reinoso <jrp.carlos@hotmail.com>
2021-09-13 09:21:30 +0100

    final

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,10 @@
-import { Box } from '@material-ui/core';
 import Chatbot from './chatbot';
 import Footer from './footer';
 import Navbar from './navbar';
 
 const Layout = ({ children }) => {
   return (
-    <
-    >
+    <>
       <Navbar />
       <main>{children}</main>
       <Footer />
