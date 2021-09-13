b196b9f0db1125ac082354a12abb998dea4c475c
Carlos Reinoso <jrp.carlos@hotmail.com>
2021-09-13 09:43:19 +0100

    remove link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Nunito:300,400,500,700&display=swap"
-          />
+         
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:300,400,500,700&display=swap"
             rel="stylesheet"
