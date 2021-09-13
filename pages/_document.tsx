ae922c6580811c64d1f4d101a448ce16c8bab375
Carlos Reinoso <jrp.carlos@hotmail.com>
2021-09-13 09:41:20 +0100

    cc2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Nunito:300,400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Nunito:300,400,500,700&display=swap"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:300,400,500,700&display=swap"
